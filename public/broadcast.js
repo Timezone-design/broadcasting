4d2d82e75021be3f8b005cb0380bc2a4a4d7b716
Timezonedesign <bolesalavb@gmail.com>
2021-07-12 18:13:13 -0700

    complete

--- public/broadcast.js
+++ public/broadcast.js
@@ -7,7 +7,7 @@ const config = {
   ]
 };
 const videoElement = document.querySelector("video");
-const socket = io.connect(https://mighty-brook-98105.herokuapp.com/);
+const socket = io.connect("https://mighty-brook-98105.herokuapp.com/");
  
 
 document.getElementById("start").onclick = function () {
--- public/watch.js
+++ public/watch.js
@@ -12,7 +12,7 @@ const config = {
   ]
 };
 
-const socket = io.connect(https://mighty-brook-98105.herokuapp.com/);
+const socket = io.connect("https://mighty-brook-98105.herokuapp.com/");
 const video = document.querySelector("video");
 
 document.getElementById("start").onclick = function () {
